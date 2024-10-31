b25471555b76144c62a11ef576694bd14c76492f
shaunadworatzek <66336949+shaunadworatzek@users.noreply.github.com>
2024-10-31 16:02:25 -0400

    This is my solution to coding challenge :-)

--- script/Dworatzek.R
+++ script/Dworatzek.R
@@ -1,6 +1,15 @@
-#making a fucntion to get data from BOLD and then get the Bin# and the number of species in that bin 
+#making a function to get the Bin# and the number of species in that bin with each component defined so it can be applied to multiple data sets 
 
-GetBoldData <- function() {
+GetBoldData <- function(data, bin_column, species_column) {
   
+  # Process the data to count species per bin
+  result <- data %>%
+    filter(!is.na(.data[[bin_column]])) %>%            # removing rows with NA in bin_uri
+    group_by(.data[[bin_column]]) %>%                  # grouping by bin_uri
+    summarize(Species_Count = n_distinct(.data[[species_column]]), .groups = 'drop') %>%  # Count distinct species in each bin
+    distinct(.data[[bin_column]], .keep_all = TRUE)    # keeping only unique bin_uri rows
+  
+  return(result)
 }
-  
+
+dfMice_final <- GetBoldData(dfMice, bin_column = "bin_uri", species_column = "species_name")
